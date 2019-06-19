3989748eb04596d792be4908bd87f2311f6f2fea
Bhavya Subramanian <bhsubra@microsoft.com>
2019-06-19 09:31:08 -0700

    Added tests for Delete, Backspace, Escape, Insert and Unhandled key press

--- src/Microsoft.Repl.Tests/ShellTests.cs
+++ src/Microsoft.Repl.Tests/ShellTests.cs
@@ -15,7 +15,9 @@ namespace Microsoft.Repl.Tests
         {
             string previousCommand = "set base \"https://localhost:44366/\"";
             Shell shell = CreateShell(ConsoleKey.UpArrow,
+                0,
                 previousCommand,
+                null,
                 out CancellationTokenSource cancellationTokenSource);
 
             // Verify the input buffer is empty before the UpArrow key press event
@@ -28,33 +30,138 @@ namespace Microsoft.Repl.Tests
         }
 
         [Fact]
-        public void RunAsync_WithEndKeyPress_UpdatesConsoleManagerCaretPositionToEndOfInput()
+        public void RunAsync_WithDownArrowKeyPress_UpdatesCurrentBufferWithNextCommand()
         {
-            Shell shell = CreateShell(ConsoleKey.End,
+            string nextCommand = "get";
+            Shell shell = CreateShell(ConsoleKey.DownArrow,
+                0,
                 null,
+                nextCommand,
                 out CancellationTokenSource cancellationTokenSource);
 
-            IShellState shellState = shell.ShellState;
-            shellState.InputManager.SetInput(shellState, "get");
+            // Verify the input buffer is empty before the DownArrow key press event
+            Assert.Equal(string.Empty, shell.ShellState.InputManager.GetCurrentBuffer());
 
             shell.RunAsync(cancellationTokenSource.Token);
 
-            Assert.Equal(35, shell.ShellState.ConsoleManager.CaretPosition);
+            // Verify the input buffer has previous command after the DownArrow key press event
+            Assert.Equal(nextCommand, shell.ShellState.InputManager.GetCurrentBuffer());
         }
 
         [Fact]
-        public void RunAsync_WithHomeKeyPress_UpdatesConsoleManagerCaretPositionToZero()
+        public void RunAsync_WithDeleteKeyPress_DeletesCurrentCharacterInTheInputBuffer()
         {
-            Shell shell = CreateShell(ConsoleKey.Home,
+            Shell shell = CreateShell(ConsoleKey.Delete,
+                2,
+                null,
                 null,
                 out CancellationTokenSource cancellationTokenSource);
 
+            string inputBufferTextBeforeKeyPress = "get";
+            string inputBufferTextAfterKeyPress = "ge";
+
+            IShellState shellState = shell.ShellState;
+            shellState.InputManager.SetInput(shellState, inputBufferTextBeforeKeyPress);
+
+            // Verify the input buffer contents before delete key press
+            Assert.Equal(inputBufferTextBeforeKeyPress, shell.ShellState.InputManager.GetCurrentBuffer());
+
             shell.RunAsync(cancellationTokenSource.Token);
 
-            Assert.Equal(0, shell.ShellState.ConsoleManager.CaretPosition);
+            // Verify the input buffer contents after delete key press
+            Assert.Equal(inputBufferTextAfterKeyPress, shell.ShellState.InputManager.GetCurrentBuffer());
         }
 
-        private Shell CreateShell(ConsoleKey consoleKey, string previousCommand, out CancellationTokenSource cancellationTokenSource)
+        [Fact]
+        public void RunAsync_WithBackspaceKeyPress_DeletesPreviousCharacterInTheInputBuffer()
+        {
+            Shell shell = CreateShell(ConsoleKey.Backspace,
+                2,
+                null,
+                null,
+                out CancellationTokenSource cancellationTokenSource);
+
+            string inputBufferTextBeforeKeyPress = "get";
+            string inputBufferTextAfterKeyPress = "gt";
+
+            IShellState shellState = shell.ShellState;
+            shellState.InputManager.SetInput(shellState, inputBufferTextBeforeKeyPress);
+
+            // Verify the input buffer contents before backspace key press
+            Assert.Equal(inputBufferTextBeforeKeyPress, shell.ShellState.InputManager.GetCurrentBuffer());
+
+            shell.RunAsync(cancellationTokenSource.Token);
+
+            // Verify the input buffer contents after backspace key press
+            Assert.Equal(inputBufferTextAfterKeyPress, shell.ShellState.InputManager.GetCurrentBuffer());
+        }
+
+        [Fact]
+        public void RunAsync_WithEscapeKeyPress_DeletesPreviousCharacterInTheInputBuffer()
+        {
+            Shell shell = CreateShell(ConsoleKey.Escape,
+                0,
+                null,
+                null,
+                out CancellationTokenSource cancellationTokenSource);
+
+            string inputBufferTextBeforeKeyPress = "get";
+            string inputBufferTextAfterKeyPress = string.Empty;
+
+            IShellState shellState = shell.ShellState;
+            shellState.InputManager.SetInput(shellState, inputBufferTextBeforeKeyPress);
+
+            // Verify the input buffer contents before escape key press
+            Assert.Equal(inputBufferTextBeforeKeyPress, shell.ShellState.InputManager.GetCurrentBuffer());
+
+            shell.RunAsync(cancellationTokenSource.Token);
+
+            // Verify the input buffer contents after escape key press
+            Assert.Equal(inputBufferTextAfterKeyPress, shell.ShellState.InputManager.GetCurrentBuffer());
+        }
+
+        [Fact]
+        public void RunAsync_WithInsertKeyPress_FlipsIsOverwriteModeInInputManager()
+        {
+            Shell shell = CreateShell(ConsoleKey.Insert,
+                0,
+                null,
+                null,
+                out CancellationTokenSource cancellationTokenSource);
+
+            // Verify IsOverwriteMode flag in input manager is set to true
+            Assert.False(shell.ShellState.InputManager.IsOverwriteMode);
+
+            shell.RunAsync(cancellationTokenSource.Token);
+
+            // Verify IsOverwriteMode flag in input manager is set to false
+            Assert.True(shell.ShellState.InputManager.IsOverwriteMode);
+        }
+
+        [Fact]
+        public void RunAsync_WithUnhandledKeyPress_DoesNothing()
+        {
+            Shell shell = CreateShell(ConsoleKey.F1,
+                0,
+                null,
+                null,
+                out CancellationTokenSource cancellationTokenSource);
+
+            string inputBufferText = "get";
+
+            IShellState shellState = shell.ShellState;
+            shellState.InputManager.SetInput(shellState, inputBufferText);
+
+            // Verify the input buffer contents before F1 key press
+            Assert.Equal(inputBufferText, shell.ShellState.InputManager.GetCurrentBuffer());
+
+            shell.RunAsync(cancellationTokenSource.Token);
+
+            // Verify the input buffer contents after F1 key press
+            Assert.Equal(inputBufferText, shell.ShellState.InputManager.GetCurrentBuffer());
+        }
+
+        private Shell CreateShell(ConsoleKey consoleKey, int caretPosition, string previousCommand, string nextCommand, out CancellationTokenSource cancellationTokenSource)
         {
             HttpState httpState = new HttpState();
             var defaultCommandDispatcher = DefaultCommandDispatcher.Create(httpState.GetPrompt, httpState);
@@ -65,10 +172,14 @@ namespace Microsoft.Repl.Tests
             ConsoleKeyInfo consoleKeyInfo = new ConsoleKeyInfo('\0', consoleKey, false, false, false);
             mockConsoleManager.SetupSequence(s => s.ReadKey(cancellationToken))
                         .Returns(consoleKeyInfo);
+            mockConsoleManager.Setup(s => s.CaretPosition)
+                        .Returns(caretPosition);
 
             Mock<ICommandHistory> mockCommandHistory = new Mock<ICommandHistory>();
             mockCommandHistory.Setup(s => s.GetPreviousCommand())
                         .Returns(previousCommand);
+            mockCommandHistory.Setup(s => s.GetNextCommand())
+            .Returns(nextCommand);
 
             ShellState shellState = new ShellState(defaultCommandDispatcher,
                 consoleManager: mockConsoleManager.Object,
