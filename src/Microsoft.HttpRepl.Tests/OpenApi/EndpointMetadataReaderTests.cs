7bf66e915f6e7e1b4fdbdeea241de43119b4956a
Bhavya Subramanian <bhsubra@microsoft.com>
2019-06-24 17:05:09 -0700

    Added tests for OpenApiV3EndPoint

--- src/Microsoft.HttpRepl.Tests/OpenApi/EndpointMetadataReaderTests.cs
+++ src/Microsoft.HttpRepl.Tests/OpenApi/EndpointMetadataReaderTests.cs
@@ -25,7 +25,7 @@ namespace Microsoft.HttpRepl.Tests.OpenApi
         }
 
         [Fact]
-        public void Read_WithSwaggerV2KeyInInputAndNoPaths_ReturnsEmptyListOfEndPointMetaData()
+        public void Read_WithSwaggerV2InputAndNoPaths_ReturnsEmptyListOfEndPointMetaData()
         {
             string json = @"{
   ""swagger"": ""2.0"",
@@ -33,14 +33,13 @@ namespace Microsoft.HttpRepl.Tests.OpenApi
     ""version"": ""v1""
   }
 }";
-            JObject jobject = JObject.Parse(json);
-            EndpointMetadataReader endpointMetadataReader = new EndpointMetadataReader();
+            List<EndpointMetadata> endpointMetadata = GetEndpointMetadataList(json);
 
-            Assert.Empty(endpointMetadataReader.Read(jobject));
+            Assert.Empty(endpointMetadata);
         }
 
         [Fact]
-        public void Read_WithSwaggerV2KeyInInputAndNoProperties_ReturnsEmptyListOfEndPointMetaData()
+        public void Read_WithSwaggerV2InputAndNoProperties_ReturnsEmptyListOfEndPointMetaData()
         {
             string json = @"{
   ""swagger"": ""2.0"",
@@ -50,14 +49,13 @@ namespace Microsoft.HttpRepl.Tests.OpenApi
   ""paths"": {
   }
 }";
-            JObject jobject = JObject.Parse(json);
-            EndpointMetadataReader endpointMetadataReader = new EndpointMetadataReader();
+            List<EndpointMetadata> endpointMetadata = GetEndpointMetadataList(json);
 
-            Assert.Empty(endpointMetadataReader.Read(jobject));
+            Assert.Empty(endpointMetadata);
         }
 
         [Fact]
-        public void Read_WithSwaggerV2KeyInInput_ReturnsEndpointMetadata()
+        public void Read_WithSwaggerV2Input_ReturnsEndpointMetadata()
         {
             string json = @"{
   ""swagger"": ""2.0"",
@@ -118,11 +116,9 @@ namespace Microsoft.HttpRepl.Tests.OpenApi
   }
 }";
 
-            JObject jobject = JObject.Parse(json);
-            EndpointMetadataReader endpointMetadataReader = new EndpointMetadataReader();
-            List<EndpointMetadata> endpointMetadata = endpointMetadataReader.Read(jobject).ToList();
+            List<EndpointMetadata> endpointMetadata = GetEndpointMetadataList(json);
             IReadOnlyDictionary<string, IReadOnlyDictionary<string, IReadOnlyList<Parameter>>> availableRequests = endpointMetadata[0].AvailableRequests;
-            KeyValuePair<string, IReadOnlyDictionary<string, IReadOnlyList<Parameter>>>  firstRequest = availableRequests.First();
+            KeyValuePair<string, IReadOnlyDictionary<string, IReadOnlyList<Parameter>>> firstRequest = availableRequests.First();
             KeyValuePair<string, IReadOnlyDictionary<string, IReadOnlyList<Parameter>>> secondRequest = availableRequests.ElementAt(1);
 
             Assert.Single(endpointMetadata);
@@ -131,6 +127,161 @@ namespace Microsoft.HttpRepl.Tests.OpenApi
             Assert.Equal(2, availableRequests.Count);
             Assert.Equal("get", firstRequest.Key);
             Assert.Equal("post", secondRequest.Key);
+        }
+
+        [Fact]
+        public void Read_WithOpenApiV3InputAndNoPaths_ReturnsEmptyListOfEndPointMetaData()
+        {
+            string json = @"{
+  ""openapi"": ""3.0.0"",
+  ""info"": {
+    ""version"": ""v1""
+  }
+}";
+            List<EndpointMetadata> endpointMetadata = GetEndpointMetadataList(json);
+
+            Assert.Empty(endpointMetadata);
+        }
+
+        [Fact]
+        public void Read_WithOpenApiV3InputAndNoProperties_ReturnsEmptyListOfEndPointMetaData()
+        {
+            string json = @"{
+  ""openapi"": ""3.0.0"",
+  ""info"": {
+    ""version"": ""v1""
+  },
+   ""paths"": {
+  }
+}";
+            List<EndpointMetadata> endpointMetadata = GetEndpointMetadataList(json);
+
+            Assert.Empty(endpointMetadata);
+        }
+
+        [Fact]
+        public void Read_WithOpenApiV3InputWithNoRequestBody_ReturnsEndpointMetadataWithEmptyAvailableRequests()
+        {
+            string json = @"{
+  ""openapi"": ""3.0.0"",
+  ""paths"": {
+    ""/pets"": {
+      ""post"": {
+        ""summary"": ""Create a pet"",
+        ""operationId"": ""createPets"",
+        ""responses"": {
+          ""201"": {
+            ""description"": ""Null response""
+          }
+        }
+      }
+    }
+  }
+}";
+
+            List<EndpointMetadata> endpointMetadata = GetEndpointMetadataList(json);
+
+            Assert.Single(endpointMetadata);
+            Assert.Equal("/pets", endpointMetadata[0].Path);
+            Assert.Empty(endpointMetadata[0].AvailableRequests);
+        }
+
+        [Fact]
+        public void Read_WithOpenApiV3InputWithNoContent_ReturnsEndpointMetadataWithEmptyAvailableRequests()
+        {
+            string json = @"{
+  ""openapi"": ""3.0.0"",
+  ""paths"": {
+    ""/pets"": {
+      ""post"": {
+        ""requestBody"": {
+          ""summary"": ""Create a pet"",
+          ""operationId"": ""createPets"",
+          ""responses"": {
+            ""201"": {
+              ""description"": ""Null response""
+            }
+          }
+        }
+      }
+    }
+  }
+}";
+
+            List<EndpointMetadata> endpointMetadata = GetEndpointMetadataList(json);
+
+            Assert.Single(endpointMetadata);
+            Assert.Equal("/pets", endpointMetadata[0].Path);
+            Assert.Empty(endpointMetadata[0].AvailableRequests);
+        }
+
+        [Fact]
+        public void Read_WithOpenApiV3Input_ReturnsEndpointMetadata()
+        {
+            string json = @"{
+  ""openapi"": ""3.0.0"",
+  ""paths"": {
+    ""/pets"": {
+      ""get"": {
+        ""requestBody"": {
+          ""content"": {
+            ""summary"": ""List all pets"",
+            ""operationId"": ""listPets"",
+            ""parameters"": [
+              {
+                ""name"": ""limit"",
+                ""in"": ""query"",
+                ""required"": false,
+                ""schema"": {
+                  ""type"": ""integer"",
+                  ""format"": ""int32""
+                }
+              }
+            ],
+            ""responses"": {
+              ""200"": {
+                ""description"": ""An paged array of pets""
+              }
+            }
+          }
+        }
+      },
+      ""post"": {
+        ""requestBody"": {
+          ""content"": {
+            ""summary"": ""Create a pet"",
+            ""operationId"": ""createPets"",
+            ""responses"": {
+              ""201"": {
+                ""description"": ""Null response""
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}";
+
+            List<EndpointMetadata> endpointMetadata = GetEndpointMetadataList(json);
+            IReadOnlyDictionary<string, IReadOnlyDictionary<string, IReadOnlyList<Parameter>>> availableRequests = endpointMetadata[0].AvailableRequests;
+            KeyValuePair<string, IReadOnlyDictionary<string, IReadOnlyList<Parameter>>> firstRequest = availableRequests.First();
+            KeyValuePair<string, IReadOnlyDictionary<string, IReadOnlyList<Parameter>>> secondRequest = availableRequests.ElementAt(1);
+
+            Assert.Single(endpointMetadata);
+            Assert.Equal("/pets", endpointMetadata[0].Path);
+
+            Assert.Equal(2, availableRequests.Count);
+            Assert.Equal("get", firstRequest.Key);
+            Assert.Equal("post", secondRequest.Key);
+        }
+
+        private List<EndpointMetadata> GetEndpointMetadataList(string json)
+        {
+            JObject jobject = JObject.Parse(json);
+            EndpointMetadataReader endpointMetadataReader = new EndpointMetadataReader();
+
+            return endpointMetadataReader.Read(jobject).ToList();
         }
     }
 }
