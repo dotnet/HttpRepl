fc28f77eb4cf5b8485250b770eb76d794407a97c
Tim Mulholland <timulhol@microsoft.com>
2019-07-25 16:04:36 -0700

    Changing GetColorValue to return default instead of AllowedColors.None (which is the same, really)

--- src/Microsoft.HttpRepl.Fakes/NullPreferences.cs
+++ src/Microsoft.HttpRepl.Fakes/NullPreferences.cs
@@ -12,17 +12,17 @@ namespace Microsoft.HttpRepl.Fakes
         public IReadOnlyDictionary<string, string> CurrentPreferences => null;
         public IReadOnlyDictionary<string, string> DefaultPreferences => null;
 
-        public AllowedColors GetColorValue(string preference, AllowedColors defaultValue = AllowedColors.None)
+        public AllowedColors GetColorValue(string preference, AllowedColors defaultValue = default)
         {
-            return AllowedColors.None;
+            return default;
         }
 
-        public int GetIntValue(string preference, int defaultValue = 0)
+        public int GetIntValue(string preference, int defaultValue = default)
         {
             return defaultValue;
         }
 
-        public string GetValue(string preference, string defaultValue = null)
+        public string GetValue(string preference, string defaultValue = default)
         {
             return defaultValue;
         }
