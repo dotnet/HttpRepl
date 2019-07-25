5b154fa186d4703bbe2ccf49617998663b7d440a
Tim Mulholland <timulhol@microsoft.com>
2019-07-25 15:49:34 -0700

    Changing up the way the AllowedColors enum is constructed and used

--- src/Microsoft.Repl/ConsoleHandling/AllowedColors.cs
+++ src/Microsoft.Repl/ConsoleHandling/AllowedColors.cs
@@ -5,26 +5,29 @@ using System;
 
 namespace Microsoft.Repl.ConsoleHandling
 {
+    // The values for the non-bold colors come from the ANSI escape sequences,
+    // specifically the SGR foreground codes, which can be referenced here:
+    // https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
     [Flags]
     public enum AllowedColors
     {
-        Black = 0x00,
+        None = 0x0,
+        Black = 0x1E,
         BoldBlack = Bold | Black,
-        Red = 0x01,
+        Red = 0x1F,
         BoldRed = Bold | Red,
-        Green = 0x02,
+        Green = 0x20,
         BoldGreen = Bold | Green,
-        Yellow = 0x03,
+        Yellow = 0x21,
         BoldYellow = Bold | Yellow,
-        Blue = 0x04,
+        Blue = 0x22,
         BoldBlue = Bold | Blue,
-        Magenta = 0x05,
+        Magenta = 0x23,
         BoldMagenta = Bold | Magenta,
-        Cyan = 0x06,
+        Cyan = 0x24,
         BoldCyan = Bold | Cyan,
-        White = 0x07,
+        White = 0x25,
         BoldWhite = White | Bold,
-        Bold = 0x100,
-        None = 0x99
+        Bold = 0x100
     }
 }
