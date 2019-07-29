15cb91f23e484a8ff1fc73ab20f075f541ba33ad
Bhavya Subramanian <bhsubra@microsoft.com>
2019-07-29 08:42:44 -0700

    Added SetHeaderCommandTests

---
+++ src/Microsoft.HttpRepl.IntegrationTests/Commands/SetHeaderCommandTests.cs
@@ -0,0 +1,115 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.HttpRepl.IntegrationTests.SampleApi;
+using Xunit;
+
+namespace Microsoft.HttpRepl.IntegrationTests.Commands
+{
+    public class SetHeaderCommandTests : BaseIntegrationTest, IClassFixture<HttpCommandsFixture<SampleApiServerConfig>>
+    {
+        private readonly SampleApiServerConfig _serverConfig;
+
+        public SetHeaderCommandTests(HttpCommandsFixture<SampleApiServerConfig> fixture)
+        {
+            _serverConfig = fixture.Config;
+        }
+
+        [Fact]
+        public async Task WithNameAndValueSpecified_AddsNewHeaderToListOfHeaders()
+        {
+            string scriptText = $@"set base {_serverConfig.BaseAddress}
+cd api/values
+echo on
+set header Accept application/json
+get";
+
+            string output = await RunTestScript(scriptText, _serverConfig.BaseAddress);
+
+            string expected = NormalizeOutput(@"(Disconnected)~ set base [BaseUrl]
+Using swagger metadata from [BaseUrl]/swagger/v1/swagger.json
+
+[BaseUrl]/~ cd api/values
+/api/values    [get|post]
+
+[BaseUrl]/api/values~ echo on
+Request echoing is on
+
+[BaseUrl]/api/values~ set header Accept application/json
+
+[BaseUrl]/api/values~ get
+Request to [BaseUrl]...
+
+GET /api/values HTTP/1.1
+Accept: application/json
+User-Agent: HTTP-REPL
+
+
+Response from [BaseUrl]...
+
+HTTP/1.1 200 OK
+Content-Type: application/json; charset=utf-8
+Date: [Date]
+Server: Kestrel
+Transfer-Encoding: chunked
+
+[
+  ""value1"",
+  ""value2""
+]
+
+
+[BaseUrl]/api/values~", null);
+
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public async Task WithEmptyValue_ClearsHeader()
+        {
+            string scriptText = $@"set base {_serverConfig.BaseAddress}
+cd api/values
+echo on
+set header User-Agent
+get";
+
+            string output = await RunTestScript(scriptText, _serverConfig.BaseAddress);
+
+            string expected = NormalizeOutput(@"(Disconnected)~ set base [BaseUrl]
+Using swagger metadata from [BaseUrl]/swagger/v1/swagger.json
+
+[BaseUrl]/~ cd api/values
+/api/values    [get|post]
+
+[BaseUrl]/api/values~ echo on
+Request echoing is on
+
+[BaseUrl]/api/values~ set header User-Agent
+
+[BaseUrl]/api/values~ get
+Request to [BaseUrl]...
+
+GET /api/values HTTP/1.1
+
+
+Response from [BaseUrl]...
+
+HTTP/1.1 200 OK
+Content-Type: application/json; charset=utf-8
+Date: [Date]
+Server: Kestrel
+Transfer-Encoding: chunked
+
+[
+  ""value1"",
+  ""value2""
+]
+
+
+[BaseUrl]/api/values~", null);
+
+            Assert.Equal(expected, output);
+        }
+    }
+}
