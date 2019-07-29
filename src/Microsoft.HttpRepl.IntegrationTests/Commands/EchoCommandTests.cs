dbd33d6c6ae56730cce0ff53e12c4a6b3e4484e8
Bhavya Subramanian <bhsubra@microsoft.com>
2019-07-28 17:36:57 -0700

    Added tests for echo command

---
+++ src/Microsoft.HttpRepl.IntegrationTests/Commands/EchoCommandTests.cs
@@ -0,0 +1,54 @@
+using System.Threading.Tasks;
+using Microsoft.HttpRepl.IntegrationTests.SampleApi;
+using Xunit;
+
+namespace Microsoft.HttpRepl.IntegrationTests.Commands
+{
+    public class EchoCommandTests : BaseIntegrationTest, IClassFixture<HttpCommandsFixture<SampleApiServerConfig>>
+    {
+        private readonly SampleApiServerConfig _serverConfig;
+
+        public EchoCommandTests(HttpCommandsFixture<SampleApiServerConfig> fixture)
+        {
+            _serverConfig = fixture.Config;
+        }
+
+        [Fact]
+        public async Task WithEchoOn_ShowsCorrectOutput()
+        {
+            string scriptText = $@"set base {_serverConfig.BaseAddress}
+echo on";
+
+            string output = await RunTestScript(scriptText, _serverConfig.BaseAddress);
+
+            string expected = NormalizeOutput(@"(Disconnected)~ set base [BaseUrl]
+Using swagger metadata from [BaseUrl]/swagger/v1/swagger.json
+
+[BaseUrl]/~ echo on
+Request echoing is on
+
+[BaseUrl]/~", null);
+
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public async Task WithEchoOff_ShowsCorrectOutput()
+        {
+            string scriptText = $@"set base {_serverConfig.BaseAddress}
+echo off";
+
+            string output = await RunTestScript(scriptText, _serverConfig.BaseAddress);
+
+            string expected = NormalizeOutput(@"(Disconnected)~ set base [BaseUrl]
+Using swagger metadata from [BaseUrl]/swagger/v1/swagger.json
+
+[BaseUrl]/~ echo off
+Request echoing is off
+
+[BaseUrl]/~", null);
+
+            Assert.Equal(expected, output);
+        }
+    }
+}
