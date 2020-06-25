547eb7328a60ce74a3526214798fb3e3ddf0b1e8
Tim Mulholland <timulhol@microsoft.com>
2020-06-25 16:02:02 -0700

    Fixing WellKnownHeaders

--- src/Microsoft.HttpRepl/WellKnownHeaders.cs
+++ src/Microsoft.HttpRepl/WellKnownHeaders.cs
@@ -1,17 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using Microsoft.HttpRepl.Resources;
 
 namespace Microsoft.HttpRepl
 {
     public static class WellKnownHeaders
     {
+        public static readonly string ContentType = "Content-Type";
+
         public static readonly IEnumerable<string> CommonHeaders = new[]
         {
             "A-IM",
@@ -91,7 +88,5 @@ namespace Microsoft.HttpRepl
 
             "Last-Modified",
         };
-
-        public static readonly string ContentType = "Content-Type";
     }
 }
