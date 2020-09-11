8fc1c9cccc67f90673078fe3860844afcf49924e
Tim Mulholland <timulhol@microsoft.com>
2020-09-10 21:42:10 -0700

    Hash unknown preference values to avoid collecting personal data inadvertently

--- src/Microsoft.HttpRepl/Telemetry/Events/PreferenceEvent.cs
+++ src/Microsoft.HttpRepl/Telemetry/Events/PreferenceEvent.cs
@@ -1,6 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Linq;
+using Microsoft.HttpRepl.Preferences;
+
 namespace Microsoft.HttpRepl.Telemetry.Events
 {
     internal class PreferenceEvent : TelemetryEventBase
@@ -8,7 +12,17 @@ namespace Microsoft.HttpRepl.Telemetry.Events
         public PreferenceEvent(string getOrSet, string preferenceName) : base("Preference")
         {
             SetProperty("GetOrSet", getOrSet);
-            SetProperty("PreferenceName", preferenceName);
+            SetProperty("PreferenceName", SanitizePreferenceName(preferenceName));
+        }
+
+        private static string SanitizePreferenceName(string preferenceName)
+        {
+            if (WellKnownPreference.Catalog.Names.Contains(preferenceName, StringComparer.OrdinalIgnoreCase))
+            {
+                return preferenceName;
+            }
+
+            return Sha256Hasher.Hash(preferenceName);
         }
     }
 }
--- test/Microsoft.HttpRepl.Tests/Commands/PrefCommandTests.cs
+++ test/Microsoft.HttpRepl.Tests/Commands/PrefCommandTests.cs
@@ -9,6 +9,7 @@ using Microsoft.HttpRepl.Commands;
 using Microsoft.HttpRepl.Fakes;
 using Microsoft.HttpRepl.FileSystem;
 using Microsoft.HttpRepl.Preferences;
+using Microsoft.HttpRepl.Telemetry;
 using Microsoft.HttpRepl.Tests.Preferences;
 using Microsoft.HttpRepl.UserProfile;
 using Microsoft.Repl.Parsing;
@@ -190,7 +191,7 @@ namespace Microsoft.HttpRepl.Tests.Commands
         [Fact]
         public async Task ExecuteAsync_WithGet_SendsTelemetry()
         {
-            Arrange("pref get preferenceName",
+            Arrange($"pref get {WellKnownPreference.DefaultEditorCommand}",
                     out HttpState httpState,
                     out MockedShellState shellState,
                     out ICoreParseResult parseResult,
@@ -206,11 +207,33 @@ namespace Microsoft.HttpRepl.Tests.Commands
             TelemetryCollector.CollectedTelemetry collectedTelemetry = telemetry.Telemetry[0];
             Assert.Equal("Preference", collectedTelemetry.EventName);
             Assert.Equal("Get", collectedTelemetry.Properties["GetOrSet"]);
-            Assert.Equal("preferenceName", collectedTelemetry.Properties["PreferenceName"]);
+            Assert.Equal(WellKnownPreference.DefaultEditorCommand, collectedTelemetry.Properties["PreferenceName"]);
         }
 
         [Fact]
         public async Task ExecuteAsync_WithSet_SendsTelemetry()
+        {
+            Arrange($"pref set {WellKnownPreference.DefaultEditorCommand} value",
+                    out HttpState httpState,
+                    out MockedShellState shellState,
+                    out ICoreParseResult parseResult,
+                    out UserFolderPreferences preferences);
+
+            TelemetryCollector telemetry = new TelemetryCollector();
+
+            PrefCommand command = new PrefCommand(preferences, telemetry);
+
+            await command.ExecuteAsync(shellState, httpState, parseResult, CancellationToken.None);
+
+            Assert.Single(telemetry.Telemetry);
+            TelemetryCollector.CollectedTelemetry collectedTelemetry = telemetry.Telemetry[0];
+            Assert.Equal("Preference", collectedTelemetry.EventName);
+            Assert.Equal("Set", collectedTelemetry.Properties["GetOrSet"]);
+            Assert.Equal(WellKnownPreference.DefaultEditorCommand, collectedTelemetry.Properties["PreferenceName"]);
+        }
+
+        [Fact]
+        public async Task ExecuteAsync_WithGetAndUnknownName_SendsTelemetryWithHashedName()
         {
             Arrange("pref set preferenceName value",
                     out HttpState httpState,
@@ -228,7 +251,7 @@ namespace Microsoft.HttpRepl.Tests.Commands
             TelemetryCollector.CollectedTelemetry collectedTelemetry = telemetry.Telemetry[0];
             Assert.Equal("Preference", collectedTelemetry.EventName);
             Assert.Equal("Set", collectedTelemetry.Properties["GetOrSet"]);
-            Assert.Equal("preferenceName", collectedTelemetry.Properties["PreferenceName"]);
+            Assert.Equal(Sha256Hasher.Hash("preferenceName"), collectedTelemetry.Properties["PreferenceName"]);
         }
 
         private static async Task ValidatePreference(string commandText, string preferenceName, Func<HttpState, string> expectedValueCallback)
