9693aa928b0882c45c7b70a2325dc3c11dfbd1e3
Ygal Bellaiche <ygal.bellaiche@sap.com>
2020-11-25 22:12:54 +0200

    remove double file

--- src/Microsoft.HttpRepl/Telemetry/Events/SetHeaderEvent.cs
+++ src/Microsoft.HttpRepl/Telemetry/Events/SetHeaderEvent.cs
@@ -25,24 +25,4 @@ namespace Microsoft.HttpRepl.Telemetry.Events
             return Sha256Hasher.Hash(headerName);
         }
     }
-
-
-    internal class SetQueryStringEvent : TelemetryEventBase
-    {
-        public SetQueryStringEvent(string key, bool isValueEmpty) : base(TelemetryEventNames.SetQueryString)
-        {
-            SetProperty(TelemetryPropertyNames.SetQueryString_Key, SanitizeKey(key));
-            SetProperty(TelemetryPropertyNames.SetQueryString_IsValueEmpty, isValueEmpty);
-        }
-
-        private static string SanitizeKey(string headerName)
-        {
-            if (string.IsNullOrEmpty(headerName))
-            {
-                return headerName;
-            }
-
-            return Sha256Hasher.Hash(headerName);
-        }
-    }
 }
